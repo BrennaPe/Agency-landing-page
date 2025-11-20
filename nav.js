e340d8475c14b6c868a9749f8af82992daef3efc
BrennaPe <brennape47@icloud.com>
2025-11-20 23:03:43 +0000

    Add mobile navigation toggle and update styles
    Introduced nav.js to handle mobile navigation menu toggling via the hamburger icon. Updated index.html to include the script and adjust navigation markup. Modified tablet and desktop CSS to support the new navigation behavior and improve responsive layout.

---
+++ nav.js
@@ -0,0 +1,9 @@
+function navOptions() {
+    let navegation = document.getElementById('navOpt');
+
+    if (navegation.style.display === 'flex') {
+        navegation.style.display = 'none';
+    } else {
+        navegation.style.display = 'flex';
+    }
+}
